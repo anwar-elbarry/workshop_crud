c255bc9cf8af692d9325230cc652adfa4684f5b6
anouar el barry <elbarryanwar37@gmail.com>
2025-03-19 10:27:10 +0000

    create store method

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,4 +10,19 @@ class BookController extends Controller
     public function index(){
         return Book::all();
     }
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+
+            $book = Book::create([
+                'name' => $request->name,
+                'author' => $request->author,
+                'description' => $request->description
+            ]);
+      
+        return response()->json(['book' => $book]);
+    }
 }
